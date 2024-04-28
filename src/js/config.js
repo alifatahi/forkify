1b26eb7003612a7c2a378a6564a4b7fd485d8c2f
Home <home@Homes-MacBook-Pro.local>
2024-04-28 21:15:39 +0330

    initail commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5e12189a-cbe7-4e21-a0d8-f27d99b19d6d';
+export const MODAL_CLOSE_SEC = 2.5;
